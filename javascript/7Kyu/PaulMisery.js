b644911bf13dccb82d4ab278b720c6ba72d0d42d
rafmasloman <raflymasloman20@gmail.com>
2021-04-17 12:46:33 +0800

    Add Solving Problem of Paul's Misery

--- javascript/7Kyu/PaulMisery.js
+++ javascript/7Kyu/PaulMisery.js
@@ -3,8 +3,41 @@ function paul(x) {
   let point = 0;
   let score = 0;
   for (let i = 0; i < x.length; i++) {
-    const element = x[i];
+    switch (x[i]) {
+      case "kata":
+        point += 5;
+        break;
+      case "life":
+        point += 0;
+        break;
+      case "Petes kata":
+        point += 10;
+        break;
+      case "eating":
+        point += 1;
+        break;
+      default:
+        break;
+    }
+  }
+  if (point < 40) {
+    return "Super happy!";
+  } else if (point < 70 && point >= 40) {
+    return "Happy!";
+  } else if (point < 100 && point >= 70) {
+    return "Sad!";
+  } else if (point > 100) {
+    return "Miserable!";
   }
 }
 
-paul(["life", "eating", "life"]);
+console.log(
+  paul([
+    "Petes kata",
+    "Petes kata",
+    "eating",
+    "Petes kata",
+    "Petes kata",
+    "eating",
+  ])
+);
